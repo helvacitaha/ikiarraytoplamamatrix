1c1d99c010573304dfde2aaab7b19873fc36e261
Taha Helvacioglu <38880784+helvacitaha@users.noreply.github.com>
2020-11-30 23:55:06 +0300

    Create kod.cpp

---
+++ kod.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main()
+{
+    int dizi1[8];
+    int dizi2[8];
+    int toplamlar[8];
+    for(int i=0;i<9;i++){
+        std::cin>>dizi1[i];
+    }for(int i=0;i<9;i++){
+        std::cin>>dizi2[i];
+    }
+    for(int i=0;i<9;i++){
+        toplamlar[i]=dizi1[i]+dizi2[i];
+    }
+    for(int i=0;i<9;i++){
+        std::cout<<toplamlar[i]<<" ";
+        if((i+1)%3==0){
+            std::cout<<std::endl;opop
+        }
+    }
+}
